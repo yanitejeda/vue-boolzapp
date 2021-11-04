587d39e9a13a7aef36fbb2473a4a78d80356eb63
yanitejeda <tejedasp@gmail.com>
2021-11-04 09:15:24 +0100

    primo step concluso da migliorare

---
+++ milestone1/script.js
@@ -0,0 +1,32 @@
+Vue.config.devtools = true,
+
+
+new Vue ({
+    el: "#root",
+    data:{
+       
+        contact:[
+           {
+            name:'Giulia',
+            avatar:'img/Giulia.png',
+            message: 'Ciao sono giulia',
+           },
+           {
+            name:'Bob',
+            avatar:'img/Bob.jpg',
+            message: 'Ciao mi chiamo Bob',
+            },
+            {
+            name:'Kevin',
+            avatar:'img/Kevin.png',
+            message: 'Blablabalbalbal banana',
+            },
+            {
+            name:'Ryan Reynolds',
+            avatar:'img/deeapool.jpg',
+            message: 'Quando hai bisogno chiama',
+            },
+            ]
+    },
+   
+})
